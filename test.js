3d8e80ef7f0caf4be58bd3de869dcc18a5ef69d4
navya-codes <nayaknavya709@gmail.com>
2025-06-28 04:41:23 +0530

    javacript basics

---
+++ test.js
@@ -0,0 +1,66 @@
+
+
+// console.log("navya")
+
+// const account_id = 1234
+// let email = "navya@gmail.com"
+// var password = "12345"
+// let state;
+// account_city = "jaipur"
+
+// //  account_id = 2; //not ALLOWED 
+
+// email = "djeed@gmail.com"
+// password = "23234"
+
+// console.log(account_id);
+
+
+// console.table([account_id, email, password, account_city,state]);
+
+// let score = 33;
+// console.log( typeof (score));
+
+// let score2 = "33";
+// console.log(typeof(score2));
+
+// let valueinnumber = Number(score2);
+// console.log(typeof(valueinnumber));
+ 
+
+
+// let score2 = undefined;
+// console.log(typeof(score2));
+
+// let valueinnumber = Number(score2);
+// console.log(typeof(valueinnumber));
+// console.log(valueinnumber);
+ 
+
+// let value = 3;
+// let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "hello" ;
+// let str2 = " navya"  ; 
+// let str3 = str1 + str2;
+
+// console.log(str3)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); 
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0); 
+
+console.log("2" === 2);
