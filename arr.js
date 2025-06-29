d67bac8033ceea912467b1fe60af659f2b120583
navya-codes <nayaknavya709@gmail.com>
2025-06-29 20:57:05 +0530

    arrays

---
+++ arr.js
@@ -0,0 +1,34 @@
+// //arrays
+
+
+const arr = [0,1,2,4,5];
+const arr2 = new Array(0,1,2,3,4);
+
+// console.log(arr[3]);
+
+// arr.push(10);
+// arr.push(55);
+// arr.push(55);
+
+// console.log(arr)   
+
+// arr.pop();
+// console.log(arr)   
+
+
+// arr.unshift(9);
+// console.log(arr) 
+
+// arr.shift();
+// console.log(arr)
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(19));
+
+
+const newarr = arr.join();
+
+console.log(arr);
+console.log(newarr);
+
+console.log( typeof newarr);
