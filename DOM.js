b0c0770ff298f4e64cab87cf5326dc0cc30c35f5
navya-codes <nayaknavya709@gmail.com>
2025-07-04 20:41:22 +0530

    Document Object Module

---
+++ DOM.js
@@ -0,0 +1,16 @@
+let divs = document.querySelectorAll(".box");
+
+let idx = 1;
+for(div of divs){
+
+    div.innerText = `new unique value ${idx}`
+    idx++;
+}
+
+// divs[0].innerText = "new unique value";
+// divs[1].innerText = "new unique value";
+// divs[2].innerText = "new unique value";
+
+// h2.innerText = h2.innerText + "from apna college students";
+
+  
---
+++ objects.js
@@ -0,0 +1,13 @@
+const jsuser = {
+
+    name: "navya",
+    age: "21",
+    location: "delhi",
+    email: "navya@gmai.com",
+    isloggedIN : false,
+    lastloggedin: ["mon" , "sat"]
+
+}
+
+console.log(jsuser.email)
+console.log(jsuser["email"])
