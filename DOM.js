badd3d62e9499c65b7251a3e893c35ee412f181b
navya-codes <nayaknavya709@gmail.com>
2025-07-04 23:55:24 +0530

    document object model pt2

--- DOM.js
+++ DOM.js
@@ -1,11 +1,11 @@
-let divs = document.querySelectorAll(".box");
+// let divs = document.querySelectorAll(".box");
 
-let idx = 1;
-for(div of divs){
+// let idx = 1;
+// for(div of divs){
 
-    div.innerText = `new unique value ${idx}`
-    idx++;
-}
+//     div.innerText = `new unique value ${idx}`
+//     idx++;
+// }
 
 // divs[0].innerText = "new unique value";
 // divs[1].innerText = "new unique value";
@@ -13,4 +13,56 @@ for(div of divs){
 
 // h2.innerText = h2.innerText + "from apna college students";
 
-  
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// let heading = document.querySelector("h1");
+// console.log(heading.getAttribute("class"));
+
+// console.log(heading.setAttribute("class" , "main head"))
+
+// let div = document.querySelector("div");
+// div.style.backgroundColor ="green"
+
+// let element = document.createElement("button");
+// element.innerText = "click me";
+
+// let div = document.querySelector("div");
+// div.append(element);
+
+// div.prepend(element);
+
+// div.before(element);
+// div.after(element);
+
+
+// let newhead = document.createElement("h1");
+// newhead.innerHTML = "<i>hello this is me</i>";
+
+// document.querySelector("body").prepend(newhead);
+
+// newhead.remove();
+
+
+
+
+
+
+// let newbtn = document.createElement("button");
+// newbtn.innerText = "click me!";
+
+
+// newbtn.style.backgroundColor = "white";
+// newbtn.style.color = "red";
+
+// document.querySelector("body").prepend(newbtn);
+
+let p1 = document.querySelector("p");
+p1.setAttribute("para1" , "para2");
+console.log(p1)
